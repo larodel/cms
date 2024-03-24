93f105b4856dc64417843d761fbe0386683736c6
larodel <164514587+larodel@users.noreply.github.com>
2024-03-24 02:54:44 -0700

    Add files via upload

---
+++ src/admin/AdminServiceProvider.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Larodel\Admin;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Route;
+
+class AdminServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        $this->registerRoutes();
+    }
+
+    private function registerRoutes()
+    {
+        Route::group([
+            'namespace' => 'Larodel\Admin\Http\Controllers',
+            'prefix' => '',
+        ], function () {
+            $this->loadRoutesFrom(__DIR__.'/routes/web.php');
+        });
+    }
+}
